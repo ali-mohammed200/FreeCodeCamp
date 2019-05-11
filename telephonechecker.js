36fd6785a77cbe2082aabeb70bf47340eb6b20f9
Mohammad Ali <ali.mohammed200@gmail.com>
2019-05-11 18:02:06 -0500

    tele

---
+++ telephonechecker.js
@@ -0,0 +1,13 @@
+function telephoneCheck(str) {
+  let val2 = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return val.test(str);
+
+
+}
+
+console.log(telephoneCheck("555-555-5555"));
+
+
+  // let val2 = /1\D?(\d{3})\D(\d{3})\D(\d{4})/;
+  // let val = /(\d{3})\D(\d{3})\D(\d{4})/;
+  // return val2.test(str) && val.test(str);
