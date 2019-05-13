95b6b1ab809767e15a695d085847692a947e483a
Mohammad Ali <ali.mohammed200@gmail.com>
2019-05-12 21:11:33 -0500

    cleaned up

--- cashregister.js
+++ cashregister.js
@@ -1,23 +1,31 @@
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.1],
+// ["QUARTER", 4.25],
+// ["ONE", 90],
+// ["FIVE", 55],
+// ["TEN", 20],
+// ["TWENTY", 60],
+// ["ONE HUNDRED", 100]]
+
 function checkCashRegister(price, cash, cid) {
   let change = (cash * 100) - (price * 100);
   let output = {status: null, change: change};
   let cashObj = cashConverter();
 
   cid.forEach(currency => {
-    // console.log(cashObj[currency[0]]["value"])
     cashObj[currency[0]]["amount"] = ((currency[1] * 100) / cashObj[currency[0]]["value"]).toFixed();
     console.log(currency[0], "Value:", cashObj[currency[0]]["value"], "Amount:", cashObj[currency[0]]["amount"])
     });
 
   let max = null;
-  let current = null;
   let tempHolder = [];
 
   let i = 0;
   while (i < cid.length){
     let curr = cid[i][0];
-    if (current === null){
-      current = cashObj[curr].value;
+    if (max === null){
       max = cashObj[curr].value;
     }
 
@@ -36,23 +44,12 @@ function checkCashRegister(price, cash, cid) {
     if (change <= 0){
       break;
     }
-    debugger;
+    // debugger;
   }
-
-  // for (let currency in cashObj) {
-  //   if(change > cashObj[currency].value){
-  //     tempHolder[currency] = cashObj[currency];
-  //     console.log(currency)
-  //   } else {
-  //     break;
-  //   }
-  // }
 
   // Here is your change, ma'am.
   return tempHolder;
 }
-
-// console.log(currency[0], cashObj[currency[0]]["amount"])
 
 function cashConverter() {
   return {
@@ -67,18 +64,6 @@ function cashConverter() {
     "ONE HUNDRED": {value: 10000, amount: null}
   };
 }
-
-
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
 
 console.log(checkCashRegister(19.84, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 console.log((2.05/0.05).toPrecision(4));
