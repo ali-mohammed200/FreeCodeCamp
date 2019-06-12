fd60690a09a2d25e6d10cfae592f7f3eb76739b1
Mohammad Ali <ali.mohammed200@gmail.com>
2019-06-12 16:23:53 -0500

    All that is left is closed drawer

--- cashregister.js
+++ cashregister.js
@@ -1,3 +1,8 @@
+// let changeObj = {DIME: 2, FIVE: 3, ONE: 1, PENNY: 4, QUARTER: 2, TEN: 2, TWENTY:3};
+// let arrChange = Object.entries(changeObj);
+// let cashConvertObj = cashConverter();
+// arrChange.sort((a, b) => cashConvertObj[a[0]].value - cashConvertObj[b[0]].value)
+
 // This one is the most closest yet. My cid for each was the one making the mistakes and the js float errors.
 // All I need now is the correct change format.
 function checkCashRegister(price, cash, cid) {
@@ -35,11 +40,11 @@ function checkCashRegister(price, cash, cid) {
 
     if (change < .01 && cashObj[curr].amount <= 0) {
       output.status = "CLOSED";
-      output.change = cashReverter(output.change);
+      output.change = objtoArrformat(cashReverter(output.change));
       break;
     } else if (change < .01){
       output.status = "OPEN"
-      output.change = cashReverter(output.change);
+      output.change = objtoArrformat(cashReverter(output.change));
       break;
     } else if (i < 0) {
       output.status = "INSUFFICIENT_FUNDS"
@@ -54,16 +59,26 @@ function checkCashRegister(price, cash, cid) {
 }
 
 function cashReverter(arr) {
-	  debugger;
-      return arr.reduce(function (allNames, name) {
-      if (name in allNames) {
-        allNames[name]++;
-      }
-      else {
-        allNames[name] = 1;
-      }
-      return allNames;
-    }, {});
+	let cashConvertObj = cashConverter();
+      let reduced = arr.reduce(function (allNames, name) {
+          if (name in allNames) {
+            allNames[name]++;
+          }
+          else {
+            allNames[name] = 1;
+          }
+          return allNames;
+        }, {});
+	for(let currency in reduced){
+		reduced[currency] = reduced[currency] * cashConvertObj[currency].value;
+	}
+	return reduced;
+}
+
+function objtoArrformat(obj) {
+	let arrChange = Object.entries(obj);
+	let cashConvertObj = cashConverter();
+ 	return arrChange.sort((a, b) => cashConvertObj[b[0]].value - cashConvertObj[a[0]].value);
 }
 
 function cashConverter() {
@@ -95,72 +110,72 @@ function cashConverter() {
 // Main functionality is done. Gives Money
 // Might be a better way to write my code with only arrays
 
-function checkCashRegister(price, cash, cid) {
-  let change = ((cash * 100) - (price * 100)).toFixed();
-  let output = {status: null, change: []};
-  let cashObj = cashConverter();
-
-  cid.forEach(currency => {
-    cashObj[currency[0]]["amount"] = ((currency[1] * 100) / cashObj[currency[0]]["value"]).toFixed();
-    console.log(currency[0], "Value:", cashObj[currency[0]]["value"], "Amount:", cashObj[currency[0]]["amount"])
-    });
-
-  let max = null;
-
-  let i = 0;
-  while (i < cid.length){
-    let curr = cid[i][0];
-    if (max === null){
-      max = cashObj[curr].value;
-    }
-
-    if (change >= cashObj[curr].value && change <= max && cashObj[curr].amount > 0){
-      output.change.push(curr);
-      cashObj[curr].amount--;
-      change -= cashObj[curr].value;
-    } else if (change >= cashObj[curr].value && change <= max && cashObj[curr].amount <= 0){
-      i -= 1;
-    } else if (change >= cashObj[curr].value){
-      max = cashObj[curr].value;
-      i += 1;
-    } else if (change <= cashObj[curr].value) {
-      max = cashObj[curr].value;
-      i -= 1;
-    }
-
-    if (change <= 0 && cashObj[curr].amount <= 0) {
-      output.status = "CLOSED";
-      break;
-    } else if (change <= 0){
-      output.status = "OPEN"
-      break;
-    } else if (i < 0) {
-      output.status = "INSUFFICIENT_FUNDS"
-      break;
-    }
-    debugger;
-  }
-
-  // Here is your change, ma'am.
-  return output;
-}
-
-function cashConverter() {
-  return {
-    "PENNY": {value: 1, amount: null},
-    "NICKEL": {value: 5, amount: null},
-    "DIME": {value: 10, amount: null},
-    "QUARTER": {value: 25, amount: null},
-    "ONE": {value: 100, amount: null},
-    "FIVE": {value: 500, amount: null},
-    "TEN": {value: 1000, amount: null},
-    "TWENTY": {value: 2000, amount: null},
-    "ONE HUNDRED": {value: 10000, amount: null}
-  };
-}
-
-console.log(checkCashRegister(19.71, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-console.log((2.05/0.05).toPrecision(4));
+// function checkCashRegister(price, cash, cid) {
+//   let change = ((cash * 100) - (price * 100)).toFixed();
+//   let output = {status: null, change: []};
+//   let cashObj = cashConverter();
+//
+//   cid.forEach(currency => {
+//     cashObj[currency[0]]["amount"] = ((currency[1] * 100) / cashObj[currency[0]]["value"]).toFixed();
+//     console.log(currency[0], "Value:", cashObj[currency[0]]["value"], "Amount:", cashObj[currency[0]]["amount"])
+//     });
+//
+//   let max = null;
+//
+//   let i = 0;
+//   while (i < cid.length){
+//     let curr = cid[i][0];
+//     if (max === null){
+//       max = cashObj[curr].value;
+//     }
+//
+//     if (change >= cashObj[curr].value && change <= max && cashObj[curr].amount > 0){
+//       output.change.push(curr);
+//       cashObj[curr].amount--;
+//       change -= cashObj[curr].value;
+//     } else if (change >= cashObj[curr].value && change <= max && cashObj[curr].amount <= 0){
+//       i -= 1;
+//     } else if (change >= cashObj[curr].value){
+//       max = cashObj[curr].value;
+//       i += 1;
+//     } else if (change <= cashObj[curr].value) {
+//       max = cashObj[curr].value;
+//       i -= 1;
+//     }
+//
+//     if (change <= 0 && cashObj[curr].amount <= 0) {
+//       output.status = "CLOSED";
+//       break;
+//     } else if (change <= 0){
+//       output.status = "OPEN"
+//       break;
+//     } else if (i < 0) {
+//       output.status = "INSUFFICIENT_FUNDS"
+//       break;
+//     }
+//     debugger;
+//   }
+//
+//   // Here is your change, ma'am.
+//   return output;
+// }
+//
+// function cashConverter() {
+//   return {
+//     "PENNY": {value: 1, amount: null},
+//     "NICKEL": {value: 5, amount: null},
+//     "DIME": {value: 10, amount: null},
+//     "QUARTER": {value: 25, amount: null},
+//     "ONE": {value: 100, amount: null},
+//     "FIVE": {value: 500, amount: null},
+//     "TEN": {value: 1000, amount: null},
+//     "TWENTY": {value: 2000, amount: null},
+//     "ONE HUNDRED": {value: 10000, amount: null}
+//   };
+// }
+//
+// console.log(checkCashRegister(19.71, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+// console.log((2.05/0.05).toPrecision(4));
 
 
 
